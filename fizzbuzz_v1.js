9dc3eef579711c0eb06ba260630d1ef41631fe90
bloobloons <elsmf.g@gmail.com>
2018-02-25 13:49:40 +0000

    a couple of FizzBuzz thoughts
    basic if-else version plus a ternary version just for fun

---
+++ fizzbuzz_v1.js
@@ -0,0 +1,20 @@
+function fizzBuzz(){
+	for(var i = 1; i <= 100; i++){
+		 if(i % 15 == 0){
+		 	console.log("FizzBuzz")
+		} else if (i % 3 == 0){
+			console.log("Fizz");
+		} else if(i % 5 == 0){
+			console.log("Buzz");
+		} else {
+			console.log(i)
+		}
+	}
+}
+
+function fizzBuzzTernary(){
+	for (let i = 1; i <= 100; i++){
+		let fizz = i % 3 ==0, buzz = i % 5 ==0;
+		console.log(fizz && buzz ? "FizzBuzz" : fizz ? "Fizz" : buzz ? "buzz" : i);
+	}
+}
